3d99d5c1aa5dbd110e410fd99cf1c830ebcd8ecd
GigaPaul <paul.vincent.web@gmail.com>
2024-07-03 16:43:59 +0200

    Player proficiency bonus

--- scripts/Playable.cs
+++ scripts/Playable.cs
@@ -4,6 +4,14 @@ using System;
 public partial class Playable : Character
 {
 	public int level = 1;
+	public int ProficiencyBonus
+	{
+		get
+		{
+			return (int)(Math.Ceiling(level / 4f) + 1);
+		}
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
